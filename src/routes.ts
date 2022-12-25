b25c1c929d46b7e23788f421fa29e639a1be8b2f
akachukwuajulibe <akachukwu-benjamin.ajulibe@epita.fr>
2022-12-25 22:56:48 +0100

    updates to routes and notifications

--- src/routes.ts
+++ src/routes.ts
@@ -4,6 +4,10 @@ import { currentUserRoutes } from '@auth/routes/currentRoutes';
 import { authMiddleware } from '@global/helpers/auth-middleware';
 import { serverAdapter } from '@service/queues/base.queue';
 import { Application } from 'express';
+import { reactionRoutes } from '@reaction/routes/reactionRoutes';
+import { notificationRoutes } from '@notification/routes/notificationRoutes';
+import { followerRoutes } from '@follower/routes/followerRoutes';
+import { commentRoutes } from '@comment/routes/commentRoutes';
 
 const BASE_PATH = '/api/v1';
 
@@ -15,6 +19,10 @@ export default (app: Application) => {
 
     app.use(BASE_PATH, authMiddleware.verifyUser, postRoutes.routes());
     app.use(BASE_PATH, authMiddleware.verifyUser, currentUserRoutes.routes());
+    app.use(BASE_PATH, authMiddleware.verifyUser, reactionRoutes.routes());
+    app.use(BASE_PATH, authMiddleware.verifyUser, commentRoutes.routes());
+    app.use(BASE_PATH, authMiddleware.verifyUser, followerRoutes.routes());
+    app.use(BASE_PATH, authMiddleware.verifyUser, notificationRoutes.routes());
   };
   routes();
 };
